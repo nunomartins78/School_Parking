875c4bb7dcdd2ea934175c6b40168a49a483b7e3
nunomartins78 <122387013+nunomartins78@users.noreply.github.com>
2023-03-03 18:21:08 +0000

    error

--- JPAExample-main/src/main/java/mindswap/jpa/entity/Manager.java
+++ JPAExample-main/src/main/java/mindswap/jpa/entity/Manager.java
@@ -10,9 +10,11 @@ import javax.persistence.Persistence;
 
 public class Manager {
     public static void main(String[] args) {
+        System.out.println("coisas");
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("school_parking");
         EntityManager em = emf.createEntityManager();
 
+        System.out.println("cenas");
         //Insert Teacher
         System.out.println("Insert new Teacher");
         em.getTransaction().begin();
